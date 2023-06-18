28070c55421a160b78cd4ac01fc1b42ec0302935
brevinhquangpham <brevinhpham3@gmail.com>
2023-06-17 23:19:41 -0400

    Implemented WelcomeScreen

--- src/main/java/cs3500/pa05/view/WeekView.java
+++ src/main/java/cs3500/pa05/view/WeekView.java
@@ -7,14 +7,13 @@ import javafx.scene.layout.GridPane;
 
 public class WeekView extends GridPane {
   public WeekView() {
-    // Define an array to represent the days of the week
+
     String[] daysOfWeek = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
 
-    // Iterate over the days of the week and add them to the grid
     for (int i = 0; i < daysOfWeek.length; i++) {
       Image img = new Image("file:images/" + daysOfWeek[i] + ".png");
       ImageView view = new ImageView(img);
-      Button button = new Button("", view); // Set the button text to empty string and the graphic to our ImageView
+      Button button = new Button("", view);
       this.add(button, i, 0);
     }
   }
