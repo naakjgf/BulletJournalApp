31a69c845d63d30e2e69a648b3e57fd7232b3699
brevinhquangpham <brevinhpham3@gmail.com>
2023-06-18 18:24:22 -0400

    Implemented render

--- src/main/java/cs3500/pa05/view/TaskView.java
+++ src/main/java/cs3500/pa05/view/TaskView.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import cs3500.pa05.model.Task;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class TaskView extends VBox {
@@ -15,9 +16,10 @@ public class TaskView extends VBox {
   public TaskView(Task task) {
     id = task.getId();
     this.dayOfWeek = task.getDayOfWeek();
-    Label nameLabel = new Label("Task: " + task.getName());
-    nameLabel.setFont(new Font("Roboto", 24));
-    Label descLabel = new Label("Description: " + task.getDescription());
+    Label nameLabel = new Label(task.getName());
+    nameLabel.setFont(new Font("Roboto", 16));
+    nameLabel.setTextFill(Color.BLACK);
+    Label descLabel = new Label(task.getDescription());
     Label statusLabel = new Label("Status: " + (task.isComplete() ? "Completed" : "Incomplete"));
 
     this.getChildren().addAll(nameLabel, descLabel, statusLabel);
