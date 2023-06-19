8cca5582421f3dca9f679928a6abcf7e4ba2340c
NeagDolph <neil@manymailz.com>
2023-06-18 20:27:43 -0400

    updates

---
+++ src/main/java/cs3500/pa05/enums/ItemAction.java
@@ -0,0 +1,7 @@
+package cs3500.pa05.enums;
+
+public enum ItemAction {
+  DELETE,
+  EDIT,
+  CLOSE
+}
--- src/main/java/cs3500/pa05/view/ScheduleItemAlert.java
+++ src/main/java/cs3500/pa05/view/ScheduleItemAlert.java
@@ -1,6 +1,8 @@
 package cs3500.pa05.view;
 
+import cs3500.pa05.enums.ItemAction;
 import cs3500.pa05.model.ScheduleItem;
+import java.util.function.Consumer;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
@@ -8,14 +10,14 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class ScheduleItemAlert extends Dialog<Void> {
-  public ScheduleItemAlert(ScheduleItem item) {
+  public ScheduleItemAlert(ScheduleItem item, Consumer<ItemAction> callback) {
     super();
     setTitle(item.getName());
     setHeaderText(null);
 
     // Create buttons
     ButtonType editButtonType = new ButtonType("Edit", ButtonBar.ButtonData.NO);
-    ButtonType deleteButtonType = new ButtonType("Delete", ButtonBar.ButtonData.NO);
+    ButtonType deleteButtonType = new ButtonType("Delete", ButtonBar.ButtonData.OK_DONE);
     ButtonType closeButtonType = new ButtonType("Close", ButtonBar.ButtonData.CANCEL_CLOSE);
     getDialogPane().getButtonTypes().addAll(editButtonType, deleteButtonType, closeButtonType);
 
